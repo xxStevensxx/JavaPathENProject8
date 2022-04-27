2f22b29630871d0fcabfae7a9d883152acd36c88
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2022-04-27 16:58:22 +0200

    Découplage du module tripPricer et Creation de l'appli tripPricer

---
+++ TripePricer/src/main/java/tripPricer/TripePricerApplication.java
@@ -0,0 +1,13 @@
+package tripPricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripePricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripePricerApplication.class, args);
+	}
+
+}
---
+++ TripePricer/src/test/java/tripPricer/TripePricerApplicationTests.java
@@ -0,0 +1,13 @@
+package tripPricer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripePricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
