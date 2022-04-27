2bc4e606aa94ab7d055b5e2296a2cd958b776375
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2022-04-27 16:36:06 +0200

    Découplage du module RewardCentral et Creation de l'appli RewardCentral

---
+++ RewardCentral/src/test/java/rewardCentral/RewardCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package rewardCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
