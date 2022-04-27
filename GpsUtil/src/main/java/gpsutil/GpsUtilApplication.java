f8df169dee859b5183f19800d16fb4b6dfa1a492
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2022-04-27 16:02:25 +0200

    Découplage du module gpsUtil et Creation de l'appli GpsUtil

---
+++ GpsUtil/src/main/java/gpsutil/GpsUtilApplication.java
@@ -0,0 +1,13 @@
+package gpsutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsUtilApplication.class, args);
+	}
+
+}
